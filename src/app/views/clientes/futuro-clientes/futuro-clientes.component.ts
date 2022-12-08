930a41c18483eea41f289146a4ea0aa46530ffaa
mitchelinoue <mitchel_inoue@msn.com>
2022-12-08 09:58:59 -0300

    criado método de busca nas tabelas da aplicação

--- src/app/views/clientes/futuro-clientes/futuro-clientes.component.ts
+++ src/app/views/clientes/futuro-clientes/futuro-clientes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 import { FuturoCliente } from 'src/app/models/futuro-cliente';
 import { FuturoClienteService } from 'src/app/services/futuro-cliente.service';
 
@@ -10,7 +11,7 @@ import { FuturoClienteService } from 'src/app/services/futuro-cliente.service';
 export class FuturoClientesComponent implements OnInit {
 
   displayedColumns: string[] = ['id', 'nome', 'cpf', 'email', 'telefone', 'editar', 'excluir'];
-  dataSource: FuturoCliente[] = [];
+  dataSource: any = [];
 
   constructor(private futuroClienteService: FuturoClienteService) { }
 
@@ -20,12 +21,18 @@ export class FuturoClientesComponent implements OnInit {
 
   public initializeTable(){
     this.futuroClienteService.findAll().subscribe(fclientes => {
-      this.dataSource = fclientes
+      this.dataSource = new MatTableDataSource(fclientes);
     })
   }
 
   public delete(id: number){
 
   }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
 
 }
