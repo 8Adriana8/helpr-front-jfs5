7b377ea0c6de8a8aafdcd75dc1f2075102c095cc
mitchelinoue <mitchel_inoue@msn.com>
2022-12-09 18:33:58 -0300

    Criado compo de upload de foto de funcionário

--- src/app/views/funcionarios/new-funcionarios/new-funcionario.component.ts
+++ src/app/views/funcionarios/new-funcionarios/new-funcionario.component.ts
@@ -3,8 +3,9 @@ import { FuncionarioService } from './../../../services/funcionario.service';
 import { Funcionario } from 'src/app/models/funcionario';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
-import { CargoService } from 'src/app/services/cargo.service';
 import { Cargo } from 'src/app/models/cargo';
+import { CargoService } from 'src/app/services/cargo.service';
+
 
 @Component({
   selector: 'app-new-funcionario',
@@ -14,13 +15,15 @@ import { Cargo } from 'src/app/models/cargo';
 export class NewfuncionarioComponent implements OnInit {
 
   public formfuncionario: FormGroup;
-  public cargos: Cargo[] = []
+  public cargos: Cargo[] = [];
+  public isLoading: Boolean = false;
+  public cover: string = ""
 
   constructor(
     formBuilder: FormBuilder,
     private FuncionarioService: FuncionarioService,
     private router: Router,
-    private cargoService: CargoService
+    private cargoService: CargoService,
   ) {
     this.formfuncionario = formBuilder.group({
       nome: ["", [Validators.required]],
@@ -33,19 +36,19 @@ export class NewfuncionarioComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.initializeCargos()
+    this.initializeCargos();
   }
 
   public initializeCargos(): void{
-this.cargoService.findAll().subscribe(response => {
-this.cargos = response
-})
+    this.cargoService.findAll().subscribe(response => {
+    this.cargos = response
+    })
   }
 
   public create(): void {
     if(this.formfuncionario.valid) {
       const funcionario: Funcionario = this.formfuncionario.value;
-      console.log(funcionario)
+
       this.FuncionarioService.create(funcionario).subscribe(() => {
         alert("funcionario cadastrado com sucesso!");
         this.router.navigate(["/funcionarios"]);
@@ -55,4 +58,25 @@ this.cargos = response
       alert("Dados inválidos.");
     }
   }
+
+  public uploadFile(event: any): void {
+    this.isLoading = true; 
+
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    
+    reader.onload = () => {
+      
+      const foto = reader.result as string;
+      this.formfuncionario.controls['foto'].setValue(foto)
+      console.log("aqui", this.formfuncionario.controls["foto"])
+    };
+
+    this.isLoading = false;
+  }
+
+
+
+
 }
