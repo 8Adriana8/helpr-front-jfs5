6d06796b2e17693d9d6bcbfd34ff7cb05c24d55c
Beatriz Araújo <b.cordeiroaraujo@gmail.com>
2022-12-09 18:45:04 -0300

    Criação de dialog para detalhes de Chamados

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -5,12 +5,14 @@ import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { DetailsComponent } from './details/details.component';
 import { DetailsCargoComponent } from './details-cargo/details-cargo.component';
+import { DetailsChamadoComponent } from './details-chamado/details-chamado.component';
 
 @NgModule({
   declarations: [
     NavBarComponent,
     DetailsComponent,
-    DetailsCargoComponent
+    DetailsCargoComponent,
+    DetailsChamadoComponent
   ],
   imports: [
     CommonModule,
