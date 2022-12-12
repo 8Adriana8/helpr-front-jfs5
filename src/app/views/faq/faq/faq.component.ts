d70048253f2cf92bf1a2bf5079259452b34831bc
mitchelinoue <mitchel_inoue@msn.com>
2022-12-12 20:09:13 -0300

    arrumado bug dark mode

--- src/app/views/faq/faq/faq.component.ts
+++ src/app/views/faq/faq/faq.component.ts
@@ -1,16 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Renderer2 } from '@angular/core';
+import { ThemeService } from 'src/app/services/theme.service';
 
 @Component({
   selector: 'app-faq',
   templateUrl: './faq.component.html',
-  styleUrls: ['./faq.component.css']
+  styleUrls: ['./faq.component.css'],
 })
 export class FaqComponent implements OnInit {
+  public switch!: boolean;
 
-  constructor() { }
+  public class = document.getElementsByTagName('app-root')[0].className;
 
-  ngOnInit(): void {
+  private unlistener!: () => void;
+
+  constructor(
+    private themeService: ThemeService,
+    private renderer2: Renderer2
+  ) {}
+
+  ngOnInit() {
+    this.unlistener = this.renderer2.listen('document', 'click', (event) => {
+      this.themeService.getData().subscribe((response) => {
+        this.switch = response;
+      });
+    });
   }
-
 }
